8860fa3de3cc1e54f444acc7dd49df116ac88c86
CEIPLSTEAM <62737128+ukathirvelu@users.noreply.github.com>
2020-04-17 12:09:25 +0530

    session given on hibernate inheritance

---
+++ Hibernate inheritance-krishnaprasad/tpcannotation/tpcannotation/src/main/java/com/javatpoint/tpcannotation/App.java
@@ -0,0 +1,13 @@
+package com.javatpoint.tpcannotation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Hibernate inheritance-krishnaprasad/tpsannotation/src/main/java/com/javatpoint/tpsannotation/App.java
@@ -0,0 +1,13 @@
+package com.javatpoint.tpsannotation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
