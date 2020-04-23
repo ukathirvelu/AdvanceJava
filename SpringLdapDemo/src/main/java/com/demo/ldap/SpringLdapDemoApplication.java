913df2d8a1ddaf220932243a25bdd94f5ba2f7c8
CEIPLSTEAM <62737128+ukathirvelu@users.noreply.github.com>
2020-04-23 20:02:06 +0530

    Spring LDAP - ARU

---
+++ SpringLdapDemo/src/main/java/com/demo/ldap/SpringLdapDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.ldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLdapDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLdapDemoApplication.class, args);
+	}
+
+}
---
+++ SpringLdapDemo/src/test/java/com/demo/ldap/SpringLdapDemoApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.demo.ldap;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringLdapDemoApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
